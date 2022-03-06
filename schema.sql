a230eeeef9ed4022b2d6ebde71cddecae93da49d
José Velasco <79725102+jovelasc@users.noreply.github.com>
2022-03-06 12:44:20 +0100

    Schema

---
+++ schema.sql
@@ -0,0 +1,37 @@
+create table company
+(
+    cif     varchar(9)   not null
+        primary key,
+    name    varchar(255) null,
+    address varchar(255) null
+);
+
+create table customer
+(
+    eth_address varchar(42)  not null
+        primary key,
+    dni         varchar(10)  null,
+    name        varchar(255) null,
+    surname     varchar(255) null,
+    email       varchar(255) null,
+    phone       varchar(20)  null,
+    gender      bit          null,
+    province    varchar(255) null,
+    city        varchar(255) null,
+    country     int          null
+);
+
+create table sla
+(
+    id       int         not null
+        primary key,
+    customer varchar(42) null,
+    company  varchar(9)  null,
+    constraint sla_fk_company
+        foreign key (company) references company (cif)
+            on update cascade on delete cascade,
+    constraint sla_fk_customer
+        foreign key (customer) references customer (eth_address)
+            on update cascade on delete cascade
+);
+
